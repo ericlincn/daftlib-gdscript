286f80c5245c63d90f05723389b125a87f0aa220
Eric Lin <mummy4@sina.com>
2023-07-01 00:01:49 +0800

    Update TransitionSprite

--- cn/daftlib/display/TransitionSprite.gd
+++ cn/daftlib/display/TransitionSprite.gd
@@ -15,32 +15,20 @@ enum Direction {
 
 signal complete
 
-var _fade_time_seconds:float
-var duration:float:
-	set(value):
-		_fade_time_seconds = value
-var _type:Type
-var type:Type:
-	set(value):
-		_type = value
-var _direction:Direction
-var direction:Direction:
-	set(value):
-		_direction = value
-var _shader_pattern:String
-var shader_pattern_path:String:
-	set(value):
-		_shader_pattern = value
+var duration:float
+var type:Type
+var direction:Direction
+var shader_pattern_path:String
 
 var _total_time:float = 0.0
 
 func _enter_tree():
-	if self._fade_time_seconds == 0: self._type = Type.INSTANT
-	if self._type == Type.INSTANT: self._fade_time_seconds = 0
+	if self.duration == 0: self.type = Type.INSTANT
+	if self.type == Type.INSTANT: self.duration = 0
 	
 	self.centered = false
 	
-	if self._type == Type.BLEND:
+	if self.type == Type.BLEND:
 		self._loadShader()
 
 func _loadShader() -> void:
@@ -49,21 +37,25 @@ func _loadShader() -> void:
 	shader_material.set_shader_parameter("dissolve_texture", texture)
 	self.material = shader_material
 
-	var shader_image = load(self._shader_pattern)
+	var shader_image = load(self.shader_pattern_path)
 	shader_material.set_shader_parameter("dissolve_texture", shader_image)
 
 func _process(delta:float) -> void:
-	if _total_time >= _fade_time_seconds:
+	if _total_time >= duration:
 		self.complete.emit()
 		queue_free()
 	
 	_total_time += delta
-	var fade_amount = _total_time / _fade_time_seconds
+	var fade_amount = _total_time / duration
 	
-	if self._direction == Direction.IN:
+	if self.direction == Direction.IN:
 		fade_amount = 1 - fade_amount
 	
-	if self._type == Type.FADE:
+	if self.type == Type.FADE:
 		self.modulate = Color(1, 1, 1, 1 - fade_amount)
-	elif self._type == Type.BLEND:
+	elif self.type == Type.BLEND:
 		self.material.set_shader_parameter("dissolve_amount", fade_amount)
+
+func _exit_tree():
+	self.texture = null
+	self.material = null
